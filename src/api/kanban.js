6d3b66ba761a9bc4b2f2e19b4451544dbaccc57e
“wenshan” <893192435@qq.com>
2020-06-20 23:00:34 +0800

    回收站

--- src/api/kanban.js
+++ src/api/kanban.js
@@ -15,10 +15,16 @@ export const listSave = (data) => request({ url: '/project-list', method: 'post'
 export const listUpdate = (data) => request({ url: '/project-list', method: 'put', data });
 // 任务列表删除
 export const listdel = (data) => request({ url: '/project-list/' + data, method: 'delete', data });
-// 更新项目列表顺序
+// 更新任务列表顺序
 export const updateListPos = (data) => request({ url: '/project-list/pos', method: 'post', data });
-// 分组查询
+//任务列表项目查询
 export const listSearch = (data) => request({ url: '/project-list/' + data, method: 'get', data });
+//只查询任务列表没有项目信息
+export const searchproject_list = (data) => request({ url: '/project-list/searchproject_list', method: 'post', data });
 
 // 更新项目顺序
 export const updatePos = (data) => request({ url: '/project/pos', method: 'post', data });
+// 更新项目顺序
+export const thoroughdle = (data) => request({ url: '/project/thoroughdle', method: 'post', data });
+// 恢复项目
+export const returnToProduct = (data) => request({ url: '/project/returnToProduct', method: 'post', data });
